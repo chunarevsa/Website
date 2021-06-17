a53d85c6aa1ec6855a0fe94265c65d59bb1bd30d
Chunarev_Sergey <chunarev.se.a@gmail.com>
2021-06-17 23:50:08 +0500

    1

---
+++ src/main/java/com/chunarevsa/Website/controllers/Controller1.java
@@ -0,0 +1,23 @@
+package com.chunarevsa.website.controllers;
+
+// копируем с https://spring.io/guides/gs/serving-web-content/
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller // класс который отвечает за обработку всех переходов на сайте
+public class Controller1 {
+
+	@GetMapping("/") // Здесь указываем какоц URL мы обрабатываем
+	// если / - то это главная страничка. 
+	// При переходе на главную страницу будет выполняться функция ниже
+	public String home (Model model) { // удаляем лишний код
+		// Ниже указваем какие данные мы можем передать внутрь шаблона 
+		model.addAttribute("title", "Главная страница"); 
+		// мы передаем параметр title со значеним Главная страница
+		return "home"; // вызываем определнный HTML шаблон
+	}
+
+}
